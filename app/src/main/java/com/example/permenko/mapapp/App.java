e2da1bde380664c229c28b0ddddaa9c7f6fa4236
Yury Permenko <permenko@gmail.com>
2018-03-19 01:29:25 +0300

    Added realm.

---
+++ app/src/main/java/com/example/permenko/mapapp/App.java
@@ -0,0 +1,11 @@
+package com.example.permenko.mapapp;
+
+import android.app.Application;
+import io.realm.Realm;
+
+public class App extends Application {
+  @Override public void onCreate() {
+    super.onCreate();
+    Realm.init(this);
+  }
+}
